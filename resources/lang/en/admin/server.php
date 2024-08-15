e4cd8955aae7888bbd66ad0442ff1e357ba02f1c
JsjaiizjsidisizkkXxd <142564837+ofccccxd8@users.noreply.github.com>
2024-08-15 10:36:50 -0400

    Update server.php

--- resources/lang/en/admin/server.php
+++ resources/lang/en/admin/server.php
@@ -2,10 +2,10 @@
 
 return [
     'exceptions' => [
-        'no_new_default_allocation' => 'You are attempting to delete the default allocation for this server but there is no fallback allocation to use.',
-        'marked_as_failed' => 'This server was marked as having failed a previous installation. Current status cannot be toggled in this state.',
-        'bad_variable' => 'There was a validation error with the :name variable.',
-        'daemon_exception' => 'There was an exception while attempting to communicate with the daemon resulting in a HTTP/:code response code. This exception has been logged. (request id: :request_id)',
+        'no_new_default_allocation' => 'Está intentando eliminar la asignación predeterminada para este servidor, pero no hay ninguna asignación alternativa para usar.',
+        'marked_as_failed' => 'Este servidor fue marcado como que falló en una instalación anterior. El estado actual no se puede alternar en este estado.',
+        'bad_variable' => 'Hubo un error de validación con el :name variable.',
+        'daemon_exception' => 'Hubo una excepción al intentar comunicarse con el demonio, lo que generó un error HTTP./:code response code. This exception has been logged. (request id: :request_id)',
         'default_allocation_not_found' => 'The requested default allocation was not found in this server\'s allocations.',
     ],
     'alerts' => [
